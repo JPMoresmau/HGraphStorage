e2bd2e70ba9d15a404d2ae47b966cef681342ea1
jpmoresmau <jp@moresmau.fr>
2015-01-14 20:37:24 +0100

    format

--- HGraphStorage/src/Database/Graph/HGraphStorage/Types.hs
+++ HGraphStorage/src/Database/Graph/HGraphStorage/Types.hs
@@ -1,24 +1,28 @@
-{-# LANGUAGE DeriveDataTypeable, DeriveGeneric, OverloadedStrings, ConstraintKinds #-}
+{-# LANGUAGE ConstraintKinds    #-}
+{-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE DeriveGeneric      #-}
+{-# LANGUAGE OverloadedStrings  #-}
 -- | Base types and simple functions on them
 module Database.Graph.HGraphStorage.Types where
 
-import Control.Exception.Base
-import Data.Binary
-import qualified Data.ByteString.Lazy as BS
-import Data.Int
-import Data.Typeable
-import Data.Default
-import qualified Data.Map as DM
-import Data.Text
+import           Control.Exception.Base
+import           Data.Binary
+import qualified Data.ByteString.Lazy                   as BS
+import           Data.Default
+import           Data.Int
+import qualified Data.Map                               as DM
+import           Data.Text
+import           Data.Typeable
 
-import GHC.Generics (Generic)
-import System.IO
-import Control.Monad.Logger (MonadLogger)
-import Control.Monad.Trans.Control ( MonadBaseControl )
-import qualified Control.Monad.Trans.Resource as R
+import           Control.Monad.Logger                   (MonadLogger)
+import           Control.Monad.Trans.Control            (MonadBaseControl)
+import qualified Control.Monad.Trans.Resource           as R
+import           GHC.Generics                           (Generic)
+import           System.IO
 
-import Database.Graph.HGraphStorage.Constants
-import Database.Graph.HGraphStorage.FreeList
+import           Database.Graph.HGraphStorage.Constants
+import           Database.Graph.HGraphStorage.FreeList
+
 
 -- | put our constraints in one synonym
 type GraphUsableMonad m=(MonadBaseControl IO m, R.MonadResource m, MonadLogger m)
@@ -53,9 +57,9 @@ type PropertyValueLength = Int64
 -- | An object as represented in the object file
 data Object = Object
   {
-    oType      :: ObjectTypeID -- ^ type of object
-  , oFirstFrom :: RelationID   -- ^ first relation starting from the object
-  , oFirstTo   :: RelationID   -- ^ first relation arriving at the object
+    oType          :: ObjectTypeID -- ^ type of object
+  , oFirstFrom     :: RelationID   -- ^ first relation starting from the object
+  , oFirstTo       :: RelationID   -- ^ first relation arriving at the object
   , oFirstProperty :: PropertyID -- ^ first property
   } deriving (Show,Read,Eq,Ord,Typeable,Generic)
 
@@ -71,38 +75,38 @@ objectSize :: Int64
 objectSize = binLength (def::Object)
 
 -- | Calculates the length of the binary serialization of the given object
-binLength :: (Binary b) => b -> Int64 
-binLength = BS.length . encode 
+binLength :: (Binary b) => b -> Int64
+binLength = BS.length . encode
 
 -- | A relation as represented in the relation file
-data Relation = Relation 
-  { rFrom      :: ObjectID  -- ^ origin object
-  , rFromType  :: ObjectTypeID -- ^ origin object type
-  , rTo        :: ObjectID -- ^ target object
-  , rToType    :: ObjectTypeID -- ^ target object type
-  , rType      :: RelationTypeID -- ^ type of the relation
-  , rFromNext  :: RelationID -- ^ next relation of origin object
-  , rToNext    :: RelationID -- ^ next relation of target object
+data Relation = Relation
+  { rFrom          :: ObjectID  -- ^ origin object
+  , rFromType      :: ObjectTypeID -- ^ origin object type
+  , rTo            :: ObjectID -- ^ target object
+  , rToType        :: ObjectTypeID -- ^ target object type
+  , rType          :: RelationTypeID -- ^ type of the relation
+  , rFromNext      :: RelationID -- ^ next relation of origin object
+  , rToNext        :: RelationID -- ^ next relation of target object
   , rFirstProperty :: PropertyID -- ^ first property id
   } deriving (Show,Read,Eq,Ord,Typeable,Generic)
 
 -- | simple binary instance
 instance Binary Relation
-  
+
 -- | simple default instance
 instance Default Relation where
   def  = Relation 0 0 0 0 0 0 0 0
-  
+
 -- | size of a relation record
 relationSize :: Int64
 relationSize =  binLength (def::Relation)
 
 -- | A property as represented in the property file
 data Property = Property
-  { pType      :: PropertyTypeID -- ^ type of the property
-  , pNext      :: PropertyID -- ^ next property id
-  , pOffset    :: PropertyValueOffset -- ^ offset of the value
-  , pLength    :: PropertyValueLength -- ^ length of the value
+  { pType   :: PropertyTypeID -- ^ type of the property
+  , pNext   :: PropertyID -- ^ next property id
+  , pOffset :: PropertyValueOffset -- ^ offset of the value
+  , pLength :: PropertyValueLength -- ^ length of the value
   } deriving (Show,Read,Eq,Ord,Typeable,Generic)
 
 -- | simple binary instance
@@ -118,7 +122,7 @@ propertySize = binLength (def::Property)
 
 -- | Type of a property as represented in the property type file
 data PropertyType = PropertyType
-  { ptDataType :: DataTypeID -- ^ Data type ID
+  { ptDataType      :: DataTypeID -- ^ Data type ID
   , ptFirstProperty :: PropertyID -- ^ first property of the type itself
   } deriving (Show,Read,Eq,Ord,Typeable,Generic)
 
@@ -144,7 +148,7 @@ instance Binary ObjectType
 -- | simple default instance
 instance Default ObjectType where
   def = ObjectType 0
-  
+
 -- | Size of an object type record
 objectTypeSize :: Int64
 objectTypeSize = binLength (def::ObjectType)
@@ -156,17 +160,17 @@ data RelationType = RelationType
 
 -- | simple binary instance
 instance Binary RelationType
-  
+
 -- | simple default instance
 instance Default RelationType where
   def = RelationType 0
-  
+
 -- | Size of a relation type record
 relationTypeSize :: Int64
 relationTypeSize = binLength (def::RelationType)
 
 -- | Handles to the various files
-data Handles = Handles 
+data Handles = Handles
   { hObjects        :: Handle
   , hObjectFree     :: FreeList ObjectID
   , hObjectTypes    :: Handle
@@ -197,7 +201,7 @@ namePropertyID = 1
 
 
 -- | A lookup table allowing two ways lookup
-data Lookup a b = Lookup 
+data Lookup a b = Lookup
   { fromName :: DM.Map b a
   , toName   :: DM.Map a b
   } deriving (Show,Read,Eq,Ord,Typeable)
@@ -223,20 +227,20 @@ dataType :: DataTypeID -> DataType
 dataType = toEnum . fromIntegral
 
 -- | A typed property value
-data PropertyValue = 
+data PropertyValue =
     PVText    Text
   | PVInteger Integer
   | PVBinary  BS.ByteString
   deriving (Show,Read,Eq,Ord,Typeable)
-  
+
 -- | Get the data type for a given value
 valueType :: PropertyValue -> DataType
 valueType (PVText _) = DTText
 valueType (PVInteger _) = DTInteger
 valueType (PVBinary _) = DTBinary
- 
+
 -- | The exceptions we may throw
-data GraphStorageException = 
+data GraphStorageException =
     IncoherentNamePropertyTypeID PropertyTypeID PropertyTypeID -- ^ Something is not right with the name property
   | UnknownPropertyType PropertyTypeID
   | NoNameProperty PropertyTypeID
@@ -244,7 +248,7 @@ data GraphStorageException =
   | UnknownObjectType ObjectTypeID
   | UnknownRelationType RelationTypeID
   deriving (Show,Read,Eq,Ord,Typeable)
-  
+
 -- | Make our exception a standard exception
 instance Exception GraphStorageException
 
@@ -258,4 +262,3 @@ data GraphSettings = GraphSettings
 -- | Default instance for settings
 instance Default GraphSettings where
   def = GraphSettings Nothing Nothing Nothing
-
