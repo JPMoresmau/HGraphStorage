299786fb0bd4445eabc2208319d72c69cf1c9507
jpmoresmau <jp@moresmau.fr>
2016-05-30 18:48:20 +0200

    transaction manager is a type class

--- LowLevelDB/src/Database/LowLevelDB/MVCC.hs
+++ LowLevelDB/src/Database/LowLevelDB/MVCC.hs
@@ -4,6 +4,7 @@
 -- <https://en.wikipedia.org/wiki/Multiversion_concurrency_control>
 module Database.LowLevelDB.MVCC
     ( TransactionManager(..)
+    , MemoryTransactionManager(..)
     , withTransactions
     , newTransaction
     , commit
@@ -37,11 +38,21 @@ data Transaction = Transaction
     , txDeleted :: Int
     } deriving (Show,Read,Eq,Ord,Typeable,Generic)
 
-data TransactionManager = TransactionManager
+data MemoryTransactionManager = MemoryTransactionManager
     {txmActive :: DS.Set Int64
     , txmAll :: DM.Map Int64 Transaction
     , txmLast :: Int64
     } deriving (Show,Read,Eq,Ord,Typeable,Generic)
+
+class (Monad m) => TransactionManager m where
+    newTx :: m Transaction
+    lastID :: m Int64
+    updateTx :: Transaction -> m ()
+    deleteTx :: Transaction -> m ()
+    getTx :: Int64 -> m (Maybe Transaction)
+
+--instance TransactionManager MemoryTransactionManager where
+
 
 data Record r = Record
     { rMin :: Int64
@@ -49,21 +60,39 @@ data Record r = Record
     , rValue :: r
     } deriving (Show,Read,Eq,Ord,Typeable,Generic)
 
-instance Default TransactionManager where
-    def = TransactionManager DS.empty DM.empty 0
+instance Default MemoryTransactionManager where
+    def = MemoryTransactionManager DS.empty DM.empty 0
 
 
 -- | Our monad transformer.
-newtype MVCCStateT m a = Gs { uns :: StateT TransactionManager m a }
+newtype MVCCStateT m a = Gs { uns :: StateT MemoryTransactionManager m a }
     deriving ( Functor, Applicative, Alternative, Monad
              , MonadFix, MonadPlus, MonadTrans )
 
-instance (Monad m) => MonadState TransactionManager (MVCCStateT m) where
+instance (Monad m) => MonadState MemoryTransactionManager (MVCCStateT m) where
     get = Gs get
     put = Gs . put
     state = Gs . state
 
-withTransactions :: MVCCStateT m a -> m (a,TransactionManager)
+instance (Monad m) =>TransactionManager (MVCCStateT m) where
+    newTx = newTransaction
+    lastID = do
+        tm <- get
+        return $ txmLast tm
+    updateTx tx = do
+        tm <- get
+        put tm{txmAll=DM.insert (txId tx) tx (txmAll tm)}
+    getTx txId = do
+        tm <- get
+        return  $ DM.lookup txId $ txmAll tm
+    deleteTx tx= do
+        tm <- get
+        let tm2 = if hasWritten tx
+                              then tm {txmAll = DM.insert (txId tx) tx (txmAll tm)}
+                              else tm {txmAll = DM.delete (txId tx) (txmAll tm)}
+        put tm2{txmActive=DS.delete (txId tx) (txmActive tm2)}
+
+withTransactions :: MVCCStateT m a -> m (a,MemoryTransactionManager)
 withTransactions st = runStateT (uns st) def
 
 newTransaction :: (Monad m)=>MVCCStateT m Transaction
@@ -77,86 +106,89 @@ newTransaction  = do
         , txmLast=next}
     return tx
 
-commit :: (Monad m) => Transaction -> MVCCStateT m ()
+commit :: (TransactionManager m) => Transaction -> m ()
 commit = closeTx Committed
 
-rollback :: (Monad m) => Transaction -> MVCCStateT m ()
+rollback :: (TransactionManager m) => Transaction -> m ()
 rollback = closeTx Aborted
 
-updateActiveTransaction :: (Monad m)=> Transaction -> MVCCStateT m ()
-updateActiveTransaction tx = do
-    tm <- get
-    put tm{txmAll=DM.insert (txId tx) tx (txmAll tm)}
+updateActiveTransaction :: (TransactionManager m)=> Transaction -> m ()
+updateActiveTransaction tx = updateTx tx
 
-writeRecord :: (Monad m) => Transaction -> [Record r] -> r -> MVCCStateT m (Transaction,[Record r])
+writeRecord :: (TransactionManager m) => Transaction -> [Record r] -> r -> m (Transaction,[Record r])
 writeRecord Transaction{..} _ _ | txStatus /= Started = error "Transaction has commited or aborted"
 writeRecord tx@Transaction{..} [] r = do
     let tx2=tx{txCreated=txCreated+1}
     updateActiveTransaction tx2
     return (tx2,[Record txId def r])
 writeRecord tx records r = do
-    tm <- get
-    let (tx2,rs) = foldr (write tm) (tx,[]) records
+    vis <- mapM (\r2->do
+        v<-isVisible tx r2
+        return (r2,v))records
+    let (tx2,rs) = foldr write (tx,[]) vis
     updateActiveTransaction tx2
     return (tx2,rs)
     where
-        write tm rec (tx1,recs) | not (isVisible tx1 tm rec) = (tx1,rec:recs)
-        write _ rec (tx1@Transaction{..},recs) =
+        write (rec,vis) (tx1,recs) | vis==False = (tx1,rec:recs)
+        write (rec,_) (tx1@Transaction{..},recs) =
             let mn= rMin rec
                 mine = txId == mn
                 newRecs = if mine then [rec{rValue=r,rMax=def}] else [Record txId def r,rec{rMax=txId}]
             in (tx1{txCreated=txCreated+1,txDeleted=txDeleted+1},newRecs++recs)
 
-deleteRecord :: (Monad m) => Transaction -> [Record r] -> MVCCStateT m (Transaction,[Record r])
+deleteRecord :: (TransactionManager m) => Transaction -> [Record r] -> m (Transaction,[Record r])
 deleteRecord Transaction{..} _  | txStatus /= Started = error "Transaction has commited or aborted"
 deleteRecord tx [] = return (tx,[])
 deleteRecord tx records = do
-    tm <- get
-    let (tx2,rs) = foldr (write tm) (tx,[]) records
+    vis <- mapM (\r->do
+        v<-isVisible tx r
+        return (r,v))records
+    let (tx2,rs) = foldr write (tx,[]) vis
     updateActiveTransaction tx2
     return (tx2,rs)
     where
-        write tm rec (tx1,recs) | not (isVisible tx1 tm rec) = (tx1,rec:recs)
-        write _ rec (tx1@Transaction{..},recs) =
+        write (rec,vis) (tx1,recs) | vis == False = (tx1,rec:recs)
+        write (rec,_) (tx1@Transaction{..},recs) =
             let mn= rMin rec
                 mine = txId == mn
                 newRecs = if mine then [] else [rec{rMax=txId}]
             in (tx1{txDeleted=txDeleted+1},newRecs++recs)
 
-readRecord :: (Monad m) => Transaction -> [Record r] -> MVCCStateT m (Maybe (Record r))
+readRecord :: (TransactionManager m) => Transaction -> [Record r] -> m (Maybe (Record r))
 readRecord _ [] = return Nothing
 readRecord tx records = do
-    tm <-get
-    return $ listToMaybe
-                $ filter (isVisible tx tm) records -- visible records
+    vis<-filterM (isVisible tx) records -- visible records
+    return $ listToMaybe vis
 
-isCommittedBefore :: Transaction -> Int64 -> TransactionManager -> Bool
-isCommittedBefore tx tId TransactionManager{..} =
-    maybe False
+isCommittedBefore :: (TransactionManager m) => Transaction -> Int64 -> m Bool
+isCommittedBefore _ tId | tId==def  = return False
+isCommittedBefore tx tId = do
+    mtx <- getTx  tId
+    return $ maybe False
         (\t->txStatus t==Committed && (txCommittedID t)<(txId tx))
-        $ DM.lookup tId txmAll
+        mtx
 
 -- |
 -- <http://momjian.us/main/writings/pgsql/mvcc.pdf>
 -- <http://momjian.us/main/writings/pgsql/internalpics.pdf>
-isVisible :: Transaction -> TransactionManager -> Record r -> Bool
-isVisible tx@Transaction{..} tm Record{..}=
-    (rMin == txId -- inserted by current transaction
-        && (rMax==def)) -- not deleted
-    || (isCommittedBefore tx rMin tm -- inserted by a commited transaction
-        && (rMax == def -- not deleted
-            || not (isCommittedBefore tx rMax tm))) -- deleted by a transaction not committed
+isVisible ::  (TransactionManager m) => Transaction -> Record r -> m Bool
+isVisible tx@Transaction{..} Record{..}= do
+    minCB <- isCommittedBefore tx rMin -- inserted by a commited transaction
+    maxCB <-isCommittedBefore tx rMax -- deleted by a committed transaction
+    return $ (rMin == txId -- inserted by current transaction
+                        && (rMax==def)) -- not deleted
+                    || (minCB -- inserted by a commited transaction
+                        && (rMax == def -- not deleted
+                            || not maxCB)) -- deleted by a transaction not committed
 
-closeTx :: (Monad m) => TransactionStatus -> Transaction -> MVCCStateT m ()
+closeTx :: (TransactionManager m) => TransactionStatus -> Transaction -> m ()
 closeTx ts tx = do
-    tm <- get
+    lastId <-  lastID
     let tx2 = if Committed == ts
-                    then tx{txStatus=ts,txCommittedID=txmLast tm}
+                    then tx{txStatus=ts,txCommittedID=lastId}
                     else tx{txStatus=ts}
-        tm2 = if hasWritten tx
-                      then tm {txmAll = DM.insert (txId tx2) tx2 (txmAll tm)}
-                      else tm {txmAll = DM.delete (txId tx) (txmAll tm)}
-    put tm2{txmActive=DS.delete (txId tx) (txmActive tm2)}
+    deleteTx tx2
+
 
 hasWritten :: Transaction -> Bool
 hasWritten Transaction{..} = txCreated>0 || txDeleted>0
